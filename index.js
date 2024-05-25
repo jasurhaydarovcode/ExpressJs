13e7c710fd78bba9a56697c80fe9d5e5d95656e3
ha7darov <vi.yeoun.vi@gmail.com>
2024-05-25 16:22:22 +0500

    middleware

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 3300;
+
+app.use((req, res, next) => {
+    console.log('Middleware 1 is running');
+    next() // next is used to move to the next middleware, keyingi middleware ni o'qib ketishi uchun
+})
+
+app.use((req, res, next) => {
+    console.log('Middleware 2');
+    res.send(`<h1><center>Middleware 2 view</center></h1>`);
+})
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`https://localhost:3300/
-Server is running on port 3300`);
+    console.log('https://localhost:3000 \nServer is running on port 3000');
 });
