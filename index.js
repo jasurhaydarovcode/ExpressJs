87e5fffd74a3df01cc246c6611b79d89c0ac9db5
ha7darov <vi.yeoun.vi@gmail.com>
2024-05-25 17:08:44 +0500

    Request body parse

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 // All Route Request Log
 app.use('/', (req, res, next) => {
-    console.log('This forever run');
     next(); 
 })
 
 // Users Page Request 
+app.use('/add-users', (req, res, next) => {
+    res.send(`
+        <form action="/users" method="POST">
+            <input type="text" name="username">
+            <input type="number" name="age">
+            <button type="submit">Send</button>
+        </form>
+    `); 
+});
+
 app.use('/users', (req, res, next) => {
-    res.send(`<p>Users List</p>`);
-})
+    console.log(req.body.username);
+    res.redirect('/');
+});
 
 // Home Page Request 
 app.use('/', (req, res, next) => {
