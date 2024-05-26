1723437834d90f51c6f8f8997605054f0af6b8c4
ha7darov <vi.yeoun.vi@gmail.com>
2024-05-27 01:32:44 +0500

    ejs

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
-const { engine } = require('express-handlebars');
+// const { engine } = require('express-handlebars');
 
 const app = express();
 
 // app.set('view engine', 'pug');
-app.engine('.hbs', engine({extname: '.hbs'}));
-app.set('view engine', '.hbs');
+// app.engine('.hbs', engine({extname: '.hbs'}));
+// app.set('view engine', '.hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.json());
@@ -19,9 +20,13 @@ const mainRoutes = require('./routes/main');
 app.use('/users', userRoutes.router);
 app.use(mainRoutes);
 
+// app.use((req, res, next) => {
+//     res.status(404).render('404'), { title: '404 Page Not Found' };
+// })
 app.use((req, res, next) => {
-    res.status(404).render('404'), { title: '404 Page Not Found' };
-})
+    res.status(404).render('404', { title: '404 Page Not Found' });
+});
+
 
 const PORT = process.env.PORT || 3300;
 
