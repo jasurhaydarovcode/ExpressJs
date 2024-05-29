b128de70cc4c78b43718cc36ce72a301e519fbee
ha7darov <vi.yeoun.vi@gmail.com>
2024-05-30 00:35:29 +0500

    Edit info & Delete User btn's added

--- controllers/users.js
+++ controllers/users.js
@@ -16,9 +16,52 @@ const getAddUsersPage = (req, res) => {
 // Description  add new user
 const addNewUser = async (req, res) => {
     try {
-        const newUser = await pool.query(`
+        await pool.query(`
             INSERT INTO user_info (username, age) VALUES ($1, $2)
         `, [req.body.username, req.body.age])
+        res.redirect('/')
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//Route         /users/edit/:id
+//Method        GET
+// Description  edit user
+const updateUserPage = async (req, res) => {
+    try {
+        const user = await pool.query('SELECT * FROM user_info WHERE id = $1', [req.params.id])
+        res.render('edit-user', {
+            title: 'Edit page',
+            user: user.rows[0]
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//Route         /users/edit/:id
+//Method        POST
+// Description  edit user
+const updateUser = async (req, res) => {
+    try {
+        await pool.query('UPDATE user_info SET username = $1, age = $2 WHERE id = $3', 
+            [req.body.username, req.body.age,  req.params.id]
+        )
+        res.redirect('/')
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//Route         /users/delete/:id
+//Method        POST
+// Description  delete user
+const deleteUser = async (req, res) => {
+    try {
+        await pool.query('DELETE FROM user_info WHERE id = $1', 
+            [req.params.id]
+        )
         res.redirect('/')
     } catch (err) {
         console.log(err);
@@ -28,4 +71,7 @@ const addNewUser = async (req, res) => {
 module.exports = {
     getAddUsersPage,
     addNewUser,
+    updateUserPage,
+    updateUser,
+    deleteUser
 }
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getAddUsersPage, addNewUser } = require('../controllers/users');
+const { 
+    getAddUsersPage, 
+    addNewUser, 
+    updateUserPage, 
+    updateUser,
+    deleteUser 
+} = require('../controllers/users');
 
 router.get('/add-users', getAddUsersPage)
+router.get('/edit/:id', updateUserPage)
 
-
+router.post('/edit/:id', updateUser)
+router.post('/delete/:id', deleteUser)
 router.post('/users', addNewUser)
 
 exports.router = router;
